69bf3b7a0e6d8e854fe4bf5554adebb3e74f4797
UpToMind <114003042+UpToMind@users.noreply.github.com>
2023-09-12 14:28:49 +0900

    fix get_element2.py

--- Original Code/get_element2.py
+++ Original Code/get_element2.py
@@ -1,13 +1,25 @@
 import numpy as np
 
+
 def get_list_element(data_list, index):
-    element = data_list[index]
-    
-    return element
+    try:
+        element = data_list[index]
+        return element
+    except IndexError:
+        print("인덱스 범위를 벗어났습니다.")
+        return None
+
 
 if __name__ == "__main__":
     my_list = np.arange(1, 100)
-    index = int(input())
+
+    while True:
+        try:
+            index = int(input("인덱스를 입력하세요: "))
+            break
+        except ValueError:
+            print("정수를 입력하세요.")
 
     result = get_list_element(my_list, index)
-    print(result)
+    if result is not None:
+        print(result)
