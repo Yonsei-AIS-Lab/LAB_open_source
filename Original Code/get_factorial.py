8b43a1c2e31b5ca2c4ce76dacc384f8e20f3f881
UpToMind <114003042+UpToMind@users.noreply.github.com>
2023-09-12 14:38:40 +0900

    fix get_factorial

--- Original Code/get_factorial.py
+++ Original Code/get_factorial.py
@@ -1,11 +1,17 @@
 if __name__ == "__main__":
-    num = int(input("양의 정수를 입력하세요: "))
+    while True:
+        try:
+            num = int(input("양의 정수를 입력하세요: "))
+            if num >= 0:
+                break
+            else:
+                print("음수는 입력할 수 없습니다. 다시 시도하세요.")
+        except ValueError:
+            print("유효한 정수를 입력하세요.")
+
     factorial = 1
 
-    if num < 0:
-        print("음수는 팩토리얼을 계산할 수 없습니다.")
-    else:
-        for i in range(1, num + 1):
-            factorial *= i
+    for i in range(1, num + 1):
+        factorial *= i
 
-        print(f"{num}! = {factorial}")
+    print(f"{num}! = {factorial}")
