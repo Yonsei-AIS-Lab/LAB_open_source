176fb98ed3e8e3cb049fe5e2915707eaebee3c23
UpToMind <114003042+UpToMind@users.noreply.github.com>
2023-09-12 14:43:00 +0900

    fix get_gcd

--- Original Code/get_gcd.py
+++ Original Code/get_gcd.py
@@ -1,13 +1,27 @@
-import numpy as np
+# 사용자가 정수가 아닌 값을 입력하면 ValueError가 발생, 예외를 처리하는 코드를 추가
+
 
 def gcd(a, b):
+    a, b = abs(a), abs(b)
     while b:
         a, b = b, a % b
     return a
 
+
 if __name__ == "__main__":
-    num1 = int(input("첫 번째 정수를 입력하세요: "))
-    num2 = int(input("두 번째 정수를 입력하세요: "))
+    while True:
+        try:
+            num1 = int(input("첫 번째 정수를 입력하세요: "))
+            break
+        except ValueError:
+            print("유효한 정수를 입력하세요.")
+
+    while True:
+        try:
+            num2 = int(input("두 번째 정수를 입력하세요: "))
+            break
+        except ValueError:
+            print("유효한 정수를 입력하세요.")
 
     gcd_result = gcd(num1, num2)
 
