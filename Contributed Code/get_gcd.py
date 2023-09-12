0cb63b9b410c51c6ed0177d40358437f5a3baa53
kindbear <dlgkdrb2@naver.com>
2023-09-12 14:16:15 +0900

    FIX: add integer exception

--- Contributed Code/get_gcd.py
+++ Contributed Code/get_gcd.py
@@ -6,9 +6,12 @@ def gcd(a, b):
     return a
 
 if __name__ == "__main__":
-    num1 = int(input("첫 번째 정수를 입력하세요: "))
-    num2 = int(input("두 번째 정수를 입력하세요: "))
+    try:
+        num1 = int(input("첫 번째 정수를 입력하세요: "))
+        num2 = int(input("두 번째 정수를 입력하세요: "))
 
-    gcd_result = gcd(num1, num2)
+        gcd_result = gcd(num1, num2)
 
-    print(f"{num1}와 {num2}의 최대 공약수는 {gcd_result}입니다.")
+        print(f"{num1}와 {num2}의 최대 공약수는 {gcd_result}입니다.")
+    except ValueError:
+        print("올바른 정수를 입력해주세요.")
