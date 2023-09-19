089c3d1bddb2b583a4b8a0156b8686e10f49a7a3
HongSeokWon <idle99970@naver.com>
2023-09-19 23:50:52 +0900

    max function's i and j position switching in line 10

--- Contributed Code/dp.py
+++ Contributed Code/dp.py
@@ -7,7 +7,7 @@ def length_of_lis(nums):
     for i in range(1, len(nums)):
         for j in range(i):
             if nums[i] > nums[j]:
-                dp[i] = max(dp[j], dp[i] + 1)
+                dp[i] = max(dp[i], dp[j] + 1)
     
     return max(dp)
 
