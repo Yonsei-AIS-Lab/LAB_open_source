5535c550773ccac146797265df412c3efcfa75df
hyoseok <alexization@gmail.com>
2023-09-19 10:54:20 +0900

    fix: 분모가 0인 경우 오류 문구와 함께 예외처리

--- Contributed Code/Week-1/divide_func.py
+++ Contributed Code/Week-1/divide_func.py
@@ -1,15 +1,18 @@
 def divide(a, b):
     try:
         result = a / b
-        return result
     except ZeroDivisionError:
-        return "0으로 나눌 수 없습니다."
+        result = "0으로 나눌 수 없습니다."
+    return result
 
 def main():
     numbers = [15, -5, 0, 12, 3]
     divisor = 2
 
-        if isinstance(result, (int, float)):
+    for num in numbers:
+        result = divide(num, divisor)
+
+        if type(result) == float:
             if result > 5:
                 print("Result is greater than 5.")
             else:
