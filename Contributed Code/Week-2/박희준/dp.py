746fb48361bb0c644201d45667eafadcae124907
Parkie <parkie0517@gmail.com>
2023-09-19 21:54:14 +0900

    Fix: fix length_of_lis function

---
+++ Contributed Code/Week-2/박희준/dp.py
@@ -0,0 +1,13 @@
+def length_of_lis(nums):
+    if not nums: # nums가 False라면 아래 라인이 실행 됨
+        return 0
+    
+    cnt = 0 # nums의 길이를 저장하기 위한 변수
+
+    for i in nums: # nums의 원소 개수만큼 iterate하며 
+        cnt += 1 # 1 증가
+
+    return cnt
+    
+# Example usage:
+print(length_of_lis([10, 9, 2, 5, 3, 7, 101, 18]))  # Output: length of the input list
