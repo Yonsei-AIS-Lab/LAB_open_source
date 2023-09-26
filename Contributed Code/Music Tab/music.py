0b929a802bd4ba09d3d45d06ceb551ccf718390f
HongSeokWon-git <idle99970@naver.com>
2023-09-26 12:32:49 +0900

    music 요소를 추가하여 모든 사운드가 아닌 배경음악에만 적용되도록 수정

--- Contributed Code/Music Tab/music.py
+++ Contributed Code/Music Tab/music.py
@@ -4,10 +4,10 @@ import pygame
 
 class Music:
     def __init__(self, file):
-        self.music = pygame.mixer.Sound(file)
+        self.music = pygame.mixer.music.load(file) # pygame.mixer.Sound는 주로 효과음에 사용되므로 배경음악 재생에 사용되는 pygame.mixer.music를 사용
 
     def play(self):
-        self.music.play()
+        self.music.play(-1) # -1은 무한 반복을 의미, 배경음악이 끝나지 않고 계속 재생되도록 함
 
     def stop(self):
-        pygame.mixer.stop()
+        pygame.mixer.music.stop() #pygame.mixer.stop은 모든 사운드를 정지시키므로 음악만 정지시키는 pygame.mixer.music.stop을 사용
