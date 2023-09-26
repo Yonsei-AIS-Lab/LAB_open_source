7230ee9bd283a068e87869ddf4696d8bb1a50829
kindbear <dlgkdrb2@naver.com>
2023-09-26 14:39:29 +0900

    FIX: add exception of place_order func and list_orders func

--- Original Code/Order Please/order_processor.py
+++ Original Code/Order Please/order_processor.py
@@ -3,6 +3,9 @@ class OrderProcessor:
         self.orders = []
 
     def place_order(self, customer, order_items):
+        if not order_items:
+            raise ValueError("주문 항목이 비어 있습니다.")
+
         total_amount = sum(item["price"] * item["quantity"] for item in order_items)
         order_id = len(self.orders) + 1
         new_order = Order(order_id, customer, total_amount)
@@ -10,5 +13,8 @@ class OrderProcessor:
         return new_order
 
     def list_orders(self):
-        for order in self.orders:
-            print(order)
+        if not self.orders:
+            print("주문이 없습니다.")
+        else:
+            for order in self.orders:
+                print(order)
