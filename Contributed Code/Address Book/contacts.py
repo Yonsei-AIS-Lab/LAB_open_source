42a7b074f8511055119dfc3255f27aac6c7c1104
kindbear <dlgkdrb2@naver.com>
2023-09-26 14:24:25 +0900

    FIX: add exception of add_contact func

--- Contributed Code/Address Book/contacts.py
+++ Contributed Code/Address Book/contacts.py
@@ -14,8 +14,11 @@ class Contacts:
     def add_contact(self, name, phone, email):
         contact = {"name": name, "phone": phone, "email": email}
         self.contacts.append(contact)
-        save_contacts(self.contacts)
-        print("연락처가 추가되었습니다.")
+        try:
+            save_contacts(self.contacts)
+            print("연락처가 추가되었습니다.")
+        except Exception as e:
+            print(f"연락처 추가 중 오류가 발생했습니다 : {e}")
 
     def search_contacts(self, search_term):
         found_contacts = [contact for contact in self.contacts if search_term.lower() in contact["name"].lower()]
