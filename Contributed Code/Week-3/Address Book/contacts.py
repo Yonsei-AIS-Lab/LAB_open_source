d27393a4c2436c2001966ce1b4e9d3aa4f8950bc
Parkie <parkie0517@gmail.com>
2023-09-26 18:47:50 +0900

    Add: add delete_contanct() to contacts.py

--- Contributed Code/Week-3/Address Book/contacts.py
+++ Contributed Code/Week-3/Address Book/contacts.py
@@ -2,10 +2,10 @@ from file_operations import read_contacts, save_contacts
 
 class Contacts:
     def __init__(self):
-        self.contacts = read_contacts()
+        self.contacts = read_contacts() # self.contacts는 'list' 타입 변수
 
     def display_contacts(self):
-        if self.contacts:
+        if self.contacts: # Python에서 리스트는 비어있을 경우 False를 반환함.
             for contact in self.contacts:
                 print(contact)
         else:
@@ -24,3 +24,10 @@ class Contacts:
                 print(contact)
         else:
             print("일치하는 연락처가 없습니다.")
+
+    def delete_contact(self, idx):
+        """
+        연락처를 삭제하는 함수를 구현하였습니다.
+        """
+        del_contact = self.contacts.pop(idx)
+        print(f'{del_contact["name"]}님의 연락처를 삭제했습니다.')
