ef24e076e5c7bd076a9133da7a516e06b6d27ac3
HongSeokWon-git <idle99970@naver.com>
2023-09-26 13:33:08 +0900

    고객 정보를 출력할 때 고객의 이름만 출력하도록 수정

--- Contributed Code/Order Please/order.py
+++ Contributed Code/Order Please/order.py
@@ -5,4 +5,7 @@ class Order:
         self.total_amount = total_amount
 
     def __str__(self):
-        return f"Order ID: {self.order_id}, Customer: {self.customer}, Total Amount: ${self.total_amount:.2f}"
+        return f"Order ID: {self.order_id}, Customer: {self.customer.name}, Total Amount: ${self.total_amount:.2f}"
+    # __str__메서드에서 주문의 고객 정보를 출력하려고 할 때 'Customer'객체를 그대로 출력하려고 하고 있으므로
+    # Customer 클래스에 __str__메서드를 이용해 고객의 이름을 출력하도록 변경
+    # 위 코드에서는 Order의 __str__ 메서드에서 self.customer 대신 self.customer.name을 사용하여 고객의 이름만 출력하도록 수정
