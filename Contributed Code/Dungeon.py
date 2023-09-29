f7b8beb51eb3f696ebeaa9d978fb7a34c7bf02c6
김현준 <hyunwns78@yonsei.ac.kr>
2023-09-29 17:57:32 +0900

    feat : 한국어 주석추가 및 시각화가

--- Contributed Code/Dungeon.py
+++ Contributed Code/Dungeon.py
@@ -1,9 +1,3 @@
-"""
-텍스트 기반 던전 크롤러 게임을 만들려고 합니다. 
-이 게임에는 플레이어와 몬스터가 등장하며, 플레이어는 몬스터를 공격하여 처치하거나 회복하여 생존해야 합니다. 
-아래는 게임을 구현하기 위한 파이썬 코드의 일부입니다.
-"""
-
 import random
 
 class Player:
@@ -12,12 +6,15 @@ class Player:
         self.health = 100
         self.inventory = []
 
+    # 플레이어의 공격 메서드. 랜덤한 데미지를 반환합니다.
     def attack(self):
         return random.randint(10, 20)
 
+    # 플레이어의 회복 메서드. 랜덤한 체력을 회복합니다.
     def heal(self):
         self.health += random.randint(10, 20)
 
+    # 플레이어가 데미지를 받는 메서드. 입력된 데미지만큼 체력을 감소시킵니다.
     def take_damage(self, damage):
         self.health -= damage
 
@@ -27,14 +24,15 @@ class Monster:
         self.health = random.randint(50, 100)
         self.attack_damage = random.randint(5, 15)
 
+    # 몬스터의 공격 메서드. 몬스터의 공격력을 반환합니다.
     def attack(self):
         return self.attack_damage
 
-    # Monster takes damage by reducing health.
+    # 몬스터가 데미지를 받는 메서드. 입력된 데미지만큼 체력을 감소시킵니다.
     def take_damage(self, damage):   
          self.health -= damage
-         if self.health < 0: # If health falls below zero,
-             self.health = 0 # set it to zero.
+         if self.health < 0: 
+             self.health = 0 
 
 def main():
     
@@ -43,29 +41,34 @@ def main():
    
    monsters = [Monster("고블린"), Monster("드래곤"), Monster("스켈레톤")]
    
-   print(f"{player.name}님의 초기 체력: {player.health}") # Print the initial health of the player
+   print("\n==== 게임 시작 ====")
+   print(f"{player.name}님의 초기 체력: {player.health}") 
    
    for monster in monsters:
-       print(f"몬스터 {monster.name}의 초기 체력: {monster.health}") # Print the initial health of each monster
+       print(f"몬스터 {monster.name}의 초기 체력: {monster.health}")
+       
+   print("=================\n")
    
    
    while player.health > 0:
-       command = input("어떤 행동을 하시겠습니까? (공격/회복/나가기): ").lower()
+       command = input("\n[명령 선택]\n1) 공격\n2) 회복\n3) 나가기\n명령어를 입력하세요: ")
+       print("-----------------\n")
+       
 
        if command == "나가기":
-            print("게임에서 나갑니다.")
+            print("\n==== 게임 종료 ====")
             
             if len(player.inventory) == 0:
-                print(f"{player.name}님이 획득한 아이템이 없습니다.") #"나가기" to print the acquired item
+                print(f"{player.name}님이 획득한 아이템이 없습니다.") 
             else:
-                print(f"{player.name}님의 승리! 획득한 아이템: {', '.join(player.inventory)}") #"나가기" to print the acquired item
+                print(f"{player.name}님의 승리! 획득한 아이템: {', '.join(player.inventory)}")
             
             return 
        
        elif command == "공격":
-            alive_monsters = [monster for monster in monsters if monster.health > 0] # Select only alive monsters
+            alive_monsters = [monster for monster in monsters if monster.health > 0] 
             
-            if not alive_monsters: # If there are no alive monsters,
+            if not alive_monsters:
                 print("모든 몬스터를 처치하셨습니다!")
                 break
             
@@ -79,7 +82,6 @@ def main():
                 print(f"{monster_to_attack.name}을(를) 처치하였습니다!")
                 player.inventory.append(f"{monster_to_attack.name}의 아이템")
                 
-                # Check again after attacking if all monsters are defeated.
                 alive_monsters_after_attack = [monster for monster in monsters if monster.health > 0]
                 
                 if not alive_monsters_after_attack: 
@@ -88,13 +90,16 @@ def main():
             
             else: 
                 for monster in monsters:
-                     print(f"몬스터 {monster.name}의 남은 체력: {monster.health}") # Print the remaining health of each monster
+                     print(f"몬스터 {monster.name}의 남은 체력: {monster.health}")
    
             
             damage_from_monster = monster_to_attack.attack()
             player.take_damage(damage_from_monster)
             
+            print("-----------------\n")
             print(f"{monster_to_attack.name}이(가) {player.name}님을 공격하여 {damage_from_monster}의 데미지를 주었습니다!")
+        
+            # 플레이어 체력 상태 출력
             print(f"플레이어 {player.name}의 남은 체력: {player.health}")
 
        elif command == "회복":
@@ -103,12 +108,16 @@ def main():
        
        else:
           print("잘못된 입력입니다. 다시 시도해주세요.")
+        
+       print("=================\n")
+        
 
+   # 게임 종료 상태 출력
    if player.health > 0:
-      print("게임이 종료되었습니다.")
+      print("\n==== 게임 종료 ====")
       print(f"{player.name}님의 승리! 획득한 아이템: {', '.join(player.inventory)}")
-   else: #player's health is 0
-      print("게임이 종료되었습니다.")
+   else:
+      print("\n==== 게임 종료 ====")
       print(f"{player.name}님이 패배하셨습니다.")
       print("아이템을 획득하지 못했습니다.")
 
