d89b72e8ad306ab0064232792436fbd8c61c3d2f
김현준 <hyunwns78@yonsei.ac.kr>
2023-09-29 23:08:26 +0900

    feat : Order Class 주석 처리

--- Contributed Code/OrderPlease/order.py
+++ Contributed Code/OrderPlease/order.py
@@ -1,8 +1,11 @@
+# 주문 클래스를 정의합니다.
 class Order:
+    # 초기화 메서드에서 주문 ID, 고객, 총 금액을 인스턴스 변수로 설정합니다.
     def __init__(self, order_id, customer, total_amount):
-        self.order_id = order_id
-        self.customer = customer
-        self.total_amount = total_amount
+        self.order_id = order_id  # 주문 ID
+        self.customer = customer  # 고객 (Customer 객체)
+        self.total_amount = total_amount  # 총 금액
 
+    # 객체를 문자열로 표현할 때 사용하는 메서드입니다.
     def __str__(self):
-        return f"Order ID: {self.order_id}, Customer: {self.customer}, Total Amount: ${self.total_amount:.2f}"
+        return f"주문 ID: {self.order_id}, 고객: {self.customer}, 총 금액: ₩{self.total_amount:.2f}"
