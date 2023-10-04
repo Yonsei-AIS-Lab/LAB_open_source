6f118f22c5f061364e4305241e95e6dbf0f6ef89
Parkie <parkie0517@gmail.com>
2023-10-04 11:13:30 +0900

    Add: add delete_contact() in contacts.py

--- Contributed Code/Week-3/Address Book/main.py
+++ Contributed Code/Week-3/Address Book/main.py
@@ -16,8 +16,9 @@ def main():
         print("주소록 관리 프로그램")
         print("1. 전체 주소록 보기")
         print("2. 주소록에 연락처 추가")
-        print("3. 주소록에서 연락처 검색")
-        print("4. 프로그램 종료")
+        print("3. 주소록에 연락처 삭제")
+        print("4. 주소록에서 연락처 검색")
+        print("5. 프로그램 종료")
 
         choice = input("선택: ")
 
@@ -28,10 +29,14 @@ def main():
             phone = input("전화번호: ")
             email = input("이메일: ")
             contacts.add_contact(name, phone, email)
-        elif choice == "3":
+        elif choice == "3": # 삭제하는 코드 추가함.
+            contacts.display_contacts()
+            delete_index = int(input('삭제하고 싶은 번호를 입력하세요: ')) - 1 # 번호를 idx로 변환하기 위해 1을 빼줌.
+            contacts.delete_contact(delete_index)
+        elif choice == "4":
             search_term = input("검색어: ")
             contacts.search_contacts(search_term)
-        elif choice == "4":
+        elif choice == "5":
             print("프로그램을 종료합니다.")
             break
         else:
