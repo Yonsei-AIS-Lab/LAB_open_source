af7b1cac274ed3c3a8d9c2110d917e3d21cefca1
hyoseok <alexization@gmail.com>
2023-10-06 17:07:14 +0900

    Feat: 할 일의 순서를 변경할 수 있는 switch_task() 함수 추가

--- Contributed Code/ToDo.py
+++ Contributed Code/ToDo.py
@@ -20,6 +20,27 @@ class ToDoList:
     def edit_task(self, task_index, new_task):
         if task_index >= 1 and task_index <= len(self.tasks):
             self.tasks[task_index - 1] = new_task
+    
+    def switch_task(self):
+        if not self.tasks:
+            return
+        
+        task_index = int(input("순서를 변경할 할 일을 선택하세요: "))
+        if task_index < 1 or task_index > len(self.tasks):
+            print("해당 번호에는 변경할 할 일이 없습니다.")
+            return
+        else:
+            task = self.tasks[task_index-1]
+
+        new_index = int(input("옮기고 싶은 순서번호를 입력해주세요: "))
+        if new_index < 1 or new_index > len(self.tasks):
+            print("해당 번호로는 변경할 수 없습니다.")
+            return
+        
+        self.tasks.insert(new_index-1, task)
+        del self.tasks[task_index]
+        print("수정 완료!")
+
 
 def main():
     to_do_list = ToDoList()
@@ -30,7 +51,8 @@ def main():
         print("2. 할 일 삭제")
         print("3. 할 일 목록 보기")
         print("4. 할 일 수정")
-        print("5. 종료")
+        print("5. 할 일 순서 변경")
+        print("6. 종료")
 
         choice = input("선택: ")
 
@@ -47,6 +69,9 @@ def main():
             new_task = input("새로운 할 일을 입력하세요: ")
             to_do_list.edit_task(task_index, new_task)
         elif choice == "5":
+            to_do_list.view_tasks()
+            to_do_list.switch_task()
+        elif choice == "6":
             print("애플리케이션을 종료합니다.")
             break
         else:
