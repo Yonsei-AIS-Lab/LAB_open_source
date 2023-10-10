ab8bf7695cce6ce7aa24107288c58f3e8eb0b264
HongSeokWon-git <idle99970@naver.com>
2023-10-10 13:53:48 +0900

    Add Password function

--- Contributed Code/bank.py
+++ Contributed Code/bank.py
@@ -1,17 +1,25 @@
 class BankAccount:
-    def __init__(self, account_number, owner, balance=0.0):
+    def __init__(self, account_number, owner, password, balance=0.0):
         self.account_number = account_number
         self.owner = owner
+        self.password = password  # 비밀번호 속성 추가
         self.balance = balance
 
-    def deposit(self, amount):
+    def deposit(self, amount, password):
+        if password != self.password:  # 비밀번호 확인
+            print("비밀번호가 틀렸습니다.")
+            return
+        
         if amount > 0:
             self.balance += amount
             print(f"{amount} 원이 입금되었습니다. 현재 잔액: {self.balance} 원")
         else:
             print("유효한 금액을 입력하세요.")
 
-    def withdraw(self, amount):
+    def withdraw(self, amount, password):
+        if password != self.password:  # 비밀번호 확인
+            print("비밀번호가 틀렸습니다.")
+            return
         if amount > 0:
             if amount <= self.balance:
                 self.balance -= amount
@@ -21,7 +29,10 @@ class BankAccount:
         else:
             print("유효한 금액을 입력하세요.")
 
-    def get_balance(self):
+    def get_balance(self, password):
+        if password != self.password:  # 비밀번호 확인
+            print("비밀번호가 틀렸습니다.")
+            return None
         return self.balance
 
     def __str__(self):
@@ -43,32 +54,37 @@ def main():
         if choice == '1':
             account_number = input("계좌번호: ")
             owner = input("소유자 이름: ")
-            account = BankAccount(account_number, owner)
+            password = input("비밀번호 설정: ")  # 비밀번호 설정 추가
+            account = BankAccount(account_number, owner, password)
             accounts.append(account)
             print("계좌가 생성되었습니다.")
         elif choice == '2':
             account_number = input("입금할 계좌번호: ")
+            password = input("비밀번호 입력: ")  # 비밀번호 입력 추가
             amount = float(input("입금할 금액: "))
             for account in accounts:
                 if account.account_number == account_number:
-                    account.deposit(amount)
+                    account.deposit(amount, password)  # 비밀번호 전달 추가
                     break
             else:
                 print("계좌를 찾을 수 없습니다.")
         elif choice == '3':
             account_number = input("출금할 계좌번호: ")
+            password = input("비밀번호 입력: ")  # 비밀번호 입력 추가
             amount = float(input("출금할 금액: "))
             for account in accounts:
                 if account.account_number == account_number:
-                    account.withdraw(amount)
+                    account.withdraw(amount, password)  # 비밀번호 전달 추가
                     break
             else:
                 print("계좌를 찾을 수 없습니다.")
         elif choice == '4':
             account_number = input("조회할 계좌번호: ")
+            password = input("비밀번호 입력: ")  # 비밀번호 입력 추가
             for account in accounts:
                 if account.account_number == account_number:
-                    print(account)
+                    if account.get_balance(password) is not None:  # 비밀번호 전달 추가
+                        print(account)
                     break
             else:
                 print("계좌를 찾을 수 없습니다.")
