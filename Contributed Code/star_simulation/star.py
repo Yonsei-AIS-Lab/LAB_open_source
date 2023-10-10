af58388c284052fb9106416ab87b376f22a30f74
KimJinSeong-Git <acmiheee@gmail.com>
2023-10-10 12:51:05 +0900

    accept: dlgkdrb

--- Contributed Code/star_simulation/star.py
+++
@@ -1,9 +0,0 @@
-class Star:
-    def __init__(self, name, mass, position, velocity):
-        self.name = name
-        self.mass = mass
-        self.position = position
-        self.velocity = velocity
-
-    def __str__(self):
-        return f"{self.name} (Mass: {self.mass}, Position: {self.position}, Velocity: {self.velocity})"
