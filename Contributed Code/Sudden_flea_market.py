dd455d9c1096533ef17e0ce3dd796d74e239ab11
황교석 <dhkk65@gmail.com>
2023-10-24 02:37:06 +0900

    수정 후 커밋

--- Contributed Code/Sudden_flea_market.py
+++ Contributed Code/Sudden_flea_market.py
@@ -20,7 +20,7 @@ guns = [
 ]
 
 player_points = 5000  # 초기 포인트 설정
-player_inventory = []  # 플레이어 보유 총 리스트
+player_inventory = {}  # 플레이어 보유 총 리스트
 
 # 메뉴 함수 정의
 def show_menu():
@@ -28,14 +28,15 @@ def show_menu():
     print("1. 총 목록 출력")
     print("2. 총 구매")
     print("3. 총 판매")
-    print("4. 종료")
+    print("4. 포인트 충전")
+    print("5. 종료")
 
 # 게임 루프
 while True:
     show_menu()
     choice = input("원하는 작업을 선택하세요: ").strip()
 
-    if choice == '4':
+    if choice == '5':
         print("게임 종료")
         break
     elif choice == '1':
@@ -55,7 +56,11 @@ while True:
                 selected_gun = guns[buy_choice - 1]
                 if player_points >= selected_gun.price:
                     player_points -= selected_gun.price
-                    player_inventory.append(selected_gun)
+                    if selected_gun.name in player_inventory:
+                        player_inventory[selected_gun.name][1] += 1 
+                    else:
+                        player_inventory[selected_gun.name] = [selected_gun, 1]
+                        #print(player_inventory[selected_gun.name][1])
                     print(f"{selected_gun.name}을(를) 구매하셨습니다!")
                 else:
                     print("포인트가 부족하여 총을 구매할 수 없습니다.")
@@ -66,22 +71,32 @@ while True:
     elif choice == '3':
         if len(player_inventory) > 0:
             print("\n판매할 총 목록:")
-            for i, gun in enumerate(player_inventory, 1):
-                print(f"{i}. {gun.name} - 가격: {gun.price // 2} 포인트")
+            for i, (_, gunL) in enumerate(player_inventory.items(), 1):
+                print(f"{i}. {gunL[0].name} - 가격: {gunL[0].price // 2} 포인트, {gunL[1]}개")
             sell_choice = input("판매할 총을 선택하세요 (또는 '취소'): ").strip()
             if sell_choice == '취소':
                 continue
             if sell_choice.isdigit():
                 sell_choice = int(sell_choice)
                 if 1 <= sell_choice <= len(player_inventory):
-                    sold_gun = player_inventory.pop(sell_choice - 1)
-                    player_points += sold_gun.price // 2
-                    print(f"{sold_gun.name}을(를) 판매하셨습니다. 받은 포인트: {sold_gun.price // 2} 포인트")
+                    selected_gun_name = list(player_inventory.keys())[sell_choice - 1]
+                    if player_inventory[selected_gun_name][1] == 1:
+                        del player_inventory[selected_gun_name]
+                    else:
+                        player_inventory[selected_gun_name][1] -= 1
                 else:
                     print("잘못된 선택입니다.")
             else:
                 print("잘못된 입력입니다. 숫자를 입력하세요.")
         else:
             print("보유한 총이 없습니다.")
+    elif choice == '4':
+        print('현재 포인트: ', player_points)
+        print('\n충전할 포인트를 입력해주세요: ', end='')
+        try:
+            point = int(input())
+            player_points += point
+        except ValueError:
+            print('잘못된 입력입니다.')
     else:
         print("잘못된 선택입니다. 다시 선택하세요.")
